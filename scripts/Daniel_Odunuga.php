308cb38ecb2aab7ea76799a64b2a1ccd58597351
Odunuga <mcdanell44@gmail.com>
2020-06-05 19:58:59 +0100

    ok working

--- scripts/Daniel_Odunuga.php
+++ scripts/Daniel_Odunuga.php
@@ -9,6 +9,6 @@ $email = "kelvind684@gmail.com";
 $language = "PHP";
 
 
-$output = "Hello World, this is $fullName with HNGi7 ID $id using $language for stage 2 task.$email";;
+$output = "Hello World, this is $fullName with HNGi7 ID $id using $language for stage 2 task.$email";
 
 echo $output;
