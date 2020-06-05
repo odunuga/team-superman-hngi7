3b0a6dbc7026317dcb7a9095bc9f910f61700518
Odunuga <mcdanell44@gmail.com>
2020-06-05 19:43:39 +0100

    ok good

--- scripts/Daniel_Odunuga.php
+++ scripts/Daniel_Odunuga.php
@@ -1,15 +1,14 @@
 <?php
-//Variables
 $name = "Odunuga Daniel";
 $id = "HNG-02440";
 
-//EMAIL
+
 $Email = "kelvind684@gmail.com";
 
 $language = "PHP";
 
 
-$result = "Hello World this is $name, my HNGi7 id is $id using $language for stage 2 task $Email";
+$result = "Hello World this is $name my HNGi7 id is $id using $language for stage 2 task $Email";
 
 echo $result;
 
