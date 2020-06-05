69167c9d32db3c2ec0723a3d471e2ab8e5ba0107
Odunuga <mcdanell44@gmail.com>
2020-06-05 16:53:25 +0100

    editing script

--- scripts/Daniel_Odunuga.php
+++ scripts/Daniel_Odunuga.php
@@ -4,6 +4,8 @@ $id = "HNG-02440";
 $Email = "kelvind684@gmail.com";
 $language = "PHP";
 echo("Hello World this is $name, my HNGi7 $id using $language for stage 2 task.$Email");
-$result = stagetwo($name, $Email, $id, $language);
-echo $result;
+echo $name;
+echo $id;
+echo $Email;
+echo $language;
 ?>
